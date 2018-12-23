724bad05395763e8265bee14fb47c45b022c6718
Martin Böhm <bohm+git@atrey.karlin.mff.cuni.cz>
2018-12-23 01:31:46 +0100

    Build script supports output directory.

--- algorithm/build.sh
+++ algorithm/build.sh
@@ -2,20 +2,28 @@
 
 usage()
 {
-	echo "usage: ./build.sh BINS R S"
-	# echo "usage: ./build.sh BINS R S [-o output-file]"
+	# echo "usage: ./build.sh BINS R S"
+	echo "usage: ./build.sh BINS R S [-odir output-dir]"
 }
 
-if [ $# -lt 3 ]; then
+if [ $# -lt 3 -o $# -eq 4 -o $# -gt 5 ]; then
 	usage
 	exit 1
 fi
 
-if [ $# -gt 4 ]; then
-	usage
-	exit 1
+if [ $# -eq 5 ]; then
+    if [ $4 != "-odir" ]; then
+      usage
+      exit 1
+    fi
 fi
 
 echo "Building for $1 bins and ratio $2/$3."
-echo "Running: mpic++ -Wall -std=c++17 -O3 -march=native -D_BINS=$1 -D_R=$2 -D_S=$3 main.cpp -o ../tests/build-$1bins-$2-$3 -pthread"
-mpic++ -Wall -std=c++17 -O3 -march=native -D_BINS=$1 -D_R=$2 -D_S=$3 main.cpp -o ../tests/build-$1-bins-$2-$3 -pthread
+
+if [ $# -eq 3 ]; then
+    echo "Running: mpic++ -Wall -std=c++17 -O3 -march=native -D_BINS=$1 -D_R=$2 -D_S=$3 main.cpp -o ../tests/build-$1bins-$2-$3 -pthread"
+    mpic++ -Wall -std=c++17 -O3 -march=native -D_BINS=$1 -D_R=$2 -D_S=$3 main.cpp -o ../tests/build-$1-bins-$2-$3 -pthread
+else
+    echo "Running: mpic++ -Wall -std=c++17 -O3 -march=native -D_BINS=$1 -D_R=$2 -D_S=$3 main.cpp -o $5/build-$1bins-$2-$3 -pthread"
+    mpic++ -Wall -std=c++17 -O3 -march=native -D_BINS=$1 -D_R=$2 -D_S=$3 main.cpp -o $5/build-$1-bins-$2-$3 -pthread
+fi
