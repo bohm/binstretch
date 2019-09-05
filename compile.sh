ca4f9ee95f72b9a139530374d37e3987e2944106
Martin Böhm <bohm+git@atrey.karlin.mff.cuni.cz>
2019-09-05 11:42:25 +0200

    The compile script can now compile only one tool.

--- compile.sh
+++ compile.sh
@@ -2,43 +2,54 @@
 
 usage()
 {
-	# echo "usage: ./build.sh BINS R S {INITIAL_SEQUENCE}"
-	echo "usage: ./build.sh BINS R S {INITIAL_SEQUENCE} [-odir output-dir]"
+	echo "usage: ./build.sh BINS R S {INITIAL_SEQUENCE} [-odir output-dir] [--search/--painter/--rooster]"
 }
 
-if [ $# -lt 3 -o $# -eq 5 -o $# -gt 6 ]; then
+if [ $# -lt 4 -o $# -gt 7 ]; then
 	usage
 	exit 1
 fi
 
-if [ $# -eq 3 ]; then
-	IS="{}"
+IS=$4
+
+if [ "$5" == "-odir" ]; then
+    OUTPUT=$6
 else 
-	IS=$4
+    OUTPUT="../build"
 fi
 
-if [ $# -eq 6 ]; then
-    if [ $5 != "-odir" ]; then
-      usage
-      exit 1
-    fi
+BUILDING_ROOSTER=true
+BUILDING_PAINTER=true
+BUILDING_SEARCH=true
+
+if [[ ( "$7" == "--search" ) || ( "$5" == "--search" ) ]]; then
+	BUILDING_ROOSTER=false
+	BUILDING_PAINTER=false
+fi
+
+if [[ ( "$7" == "--painter" ) || ( "$5" == "--painter" ) ]]; then
+	BUILDING_SEARCH=false
+	BUILDING_ROOSTER=false
+fi
+
+if [[ ( "$7" == "--rooster" ) || ( "$5" == "--rooster" ) ]]; then
+	BUILDING_SEARCH=false
+	BUILDING_PAINTER=false
 fi
 
 echo "Building for $1 bins, ratio $2/$3 and initial sequence $IS."
 
-if [ $# -eq 3 -o $# -eq 4 ]; then
-    echo "Running: mpic++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS main.cpp -o ../build/search-$1-bins-$2-$3 -pthread"
-    cd algorithm; mpic++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS main.cpp -o ../build/search-$1-bins-$2-$3 -pthread; cd ..
-    echo "Running: g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS painter.cpp -o ../build/painter-$1-bins-$2-$3 -pthread"
-    cd painter; g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS painter.cpp -o ../build/painter-$1-bins-$2-$3 -pthread; cd ..
-    echo "Running: g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS rooster.cpp -o ../build/rooster-$1-bins-$2-$3 -pthread"
-    cd painter; g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS rooster.cpp -o ../build/rooster-$1-bins-$2-$3 -pthread; cd ..
+if [[ "$BUILDING_SEARCH" = true ]]; then
+	echo "Running: mpic++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS main.cpp -o $OUTPUT/search-$1-bins-$2-$3 -pthread"
+	cd algorithm; mpic++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS main.cpp -o $OUTPUT/search-$1-bins-$2-$3 -pthread; cd ..
+fi
 
-else
-    echo "Running: mpic++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS main.cpp -o $6/search-$1-bins-$2-$3 -pthread"
-    cd algorithm; mpic++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS main.cpp -o $6/search-$1-bins-$2-$3 -pthread; cd ..
-    echo "Running: g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS painter.cpp -o $6/painter-$1-bins-$2-$3 -pthread"
-    cd painter; g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS painter.cpp -o $6/painter-$1-bins-$2-$3 -pthread; cd ..
-    echo "Running: g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS rooster.cpp -o $6/rooster-$1-bins-$2-$3 -pthread"
-    cd painter; g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS rooster.cpp -o $6/rooster-$1-bins-$2-$3 -pthread; cd ..
+if [[ "$BUILDING_PAINTER" = true ]]; then
+	echo "Running: g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS painter.cpp -o $OUTPUT/painter-$1-bins-$2-$3 -pthread"
+	cd painter; g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS painter.cpp -o $OUTPUT/painter-$1-bins-$2-$3 -pthread; cd ..
+fi
+
+if [[ "$BUILDING_ROOSTER" = true ]]; then
+    echo "Running: g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS rooster.cpp -o $OUTPUT/rooster-$1-bins-$2-$3 -pthread"
+    cd painter; g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS rooster.cpp -o $OUTPUT/rooster-$1-bins-$2-$3 -pthread; cd ..
 fi
