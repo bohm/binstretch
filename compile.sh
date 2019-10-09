544734f179a03d08b6049f830ed4dd080fd941f8
Martin Böhm <bohm+git@atrey.karlin.mff.cuni.cz>
2019-10-09 16:34:05 +0200

    Compile.sh now more consistent with the coq repository.

--- compile.sh
+++ compile.sh
@@ -2,7 +2,10 @@
 
 usage()
 {
-	echo "usage: ./build.sh BINS R S {INITIAL_SEQUENCE} [-odir output-dir] [--search/--painter/--rooster]"
+	echo "usage: ./build.sh M T G {INITIAL_SEQUENCE} [-odir output-dir] [--search/--painter/--rooster]"
+	echo "where M: the number of bins/machines (e.g. 6)"
+	echo "      T: the allowed load of bins (e.g. 19)"
+	echo "      G: the optimal maximum load of all bins (e.g. 14)"
 }
 
 if [ $# -lt 4 -o $# -gt 7 ]; then
@@ -16,6 +19,12 @@ if [ "$5" == "-odir" ]; then
     OUTPUT=$6
 else 
     OUTPUT="../build"
+fi
+
+# If the directory OUTPUT does not exist, try to create it yourself.
+
+if [ ! -d $OUTPUT ]; then
+    mkdir $OUTPUT;
 fi
 
 BUILDING_ROOSTER=true
@@ -40,16 +49,16 @@ fi
 echo "Building for $1 bins, ratio $2/$3 and initial sequence $IS."
 
 if [[ "$BUILDING_SEARCH" = true ]]; then
-	echo "Running: mpic++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS main.cpp -o $OUTPUT/search-$1-bins-$2-$3 -pthread"
-	cd algorithm; mpic++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS main.cpp -o $OUTPUT/search-$1-bins-$2-$3 -pthread; cd ..
+	echo "Running: mpic++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS main.cpp -o $OUTPUT/search-$1-$2-$3 -pthread"
+	cd algorithm; mpic++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS main.cpp -o $OUTPUT/search-$1-$2-$3 -pthread; cd ..
 fi
 
 if [[ "$BUILDING_PAINTER" = true ]]; then
-	echo "Running: g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS painter.cpp -o $OUTPUT/painter-$1-bins-$2-$3 -pthread"
-	cd painter; g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS painter.cpp -o $OUTPUT/painter-$1-bins-$2-$3 -pthread; cd ..
+	echo "Running: g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS painter.cpp -o $OUTPUT/painter-$1-$2-$3 -pthread"
+	cd painter; g++ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS painter.cpp -o $OUTPUT/painter-$1-$2-$3 -pthread; cd ..
 fi
 
 if [[ "$BUILDING_ROOSTER" = true ]]; then
     echo "Compiling converter binary for $1 bins and ratio $2/$3 bins into $OUTPUT/rooster-$1-$2-$3."
-    cd rooster; g++ -I../ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS rooster.cpp -o $OUTPUT/rooster-$1-bins-$2-$3 -pthread; cd ..
+    cd rooster; g++ -I../ -Wall -std=c++17 -O3 -march=native -DIBINS=$1 -DIR=$2 -DIS=$3 -DII_S=$IS rooster.cpp -o $OUTPUT/rooster-$1-$2-$3 -pthread; cd ..
 fi
