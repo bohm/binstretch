fa8d60045c6c7d6820f8fe59672cea1218d6617b
Martin Böhm <bohm+git@atrey.karlin.mff.cuni.cz>
2022-09-24 17:01:02 +0200

    Minor.

--- minitools/process_saplings.sh
+++ minitools/process_saplings.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-BINS=7
+BINS=10
 R=19
 S=14
-ADVICE_PREFIX=""
+ROOT_STRING="--root ./experiments/v3-subroot-1-10-19-14.txt"
+ADVICE_STRING="--advice ./experiments/v3-advice-10-19-14.txt"
+ASSUME_STRING="--assume ./experiments/v3-assumptions-big-unique-10-19-14.txt"
 TIME_SUFFIX=`date +"%Y-%m-%d %T"`
 
-./build/listsaplings-$BINS-$R-$S --advice ./experiments/${ADVICE_PREFIX}advice-$BINS-$R-$S.txt > ./experiments/temporarylist-$BINS-$R-$S.txt 2>/dev/null
+nice -n 10 ./build/listsaplings-$BINS-$R-$S $ROOT_STRING $ADVICE_STRING $ASSUME_STRING > ./experiments/temporarylist-$BINS-$R-$S.txt
 
 while read line; do
     echo "Processing sapling $line"
     echo "Processing sapling $line" >> "./logs/process-$BINS-$R-$S-$TIME_SUFFIX.txt"
     echo "$line" > ./experiments/temporarysapling.txt
-    mpirun -np 2 --bind-to none "./build/search-$BINS-$R-$S" --root ./experiments/temporarysapling.txt </dev/null 2>&1 | tee -a "./logs/process-$BINS-$R-$S-$TIME_SUFFIX.txt"
+    mpirun -np 2 --bind-to none nice -n 10 "./build/search-$BINS-$R-$S" --root ./experiments/temporarysapling.txt $ASSUME_STRING </dev/null 2>&1 | tee -a "./logs/process-$BINS-$R-$S-$TIME_SUFFIX.txt"
 done < ./experiments/temporarylist-$BINS-$R-$S.txt
