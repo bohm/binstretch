300878f85534f26b193731e6abe40d8899e54d79
Martin Böhm <bohm+git@atrey.karlin.mff.cuni.cz>
2023-08-24 17:56:37 +0200

    Added some sandbox tests regarding 'midgame-feasible' item configurations.

--- search/updater.hpp
+++ search/updater.hpp
@@ -125,7 +125,7 @@ public:
     void print_uncertain_outedges() {
         // fprintf(stderr, "It is uncertain how to solve ");
         // print_binconf_stream(stderr, job.root->bc, false);
-        fprintf(stderr, "Uncertain edges from root: ");
+        fprintf(stderr, "Uncertain edges from job root: ");
         std::vector<int> observed_outedges_vec(last_observed_outedges.begin(), last_observed_outedges.end());
         std::sort(observed_outedges_vec.begin(), observed_outedges_vec.end());
         for (int item: observed_outedges_vec)
@@ -143,8 +143,7 @@ public:
             {
                 if (!new_outedge_labels.contains(item))
                 {
-                    fprintf(stderr, "Sending item %d leads to algorithmic victory from configuration ", item);
-                    print_binconf_stream(stderr, job.root->bc, true);
+                    fprintf(stderr, "Sending item %d leads to alg victory from job root.\n", item);
                 }
             }
 
@@ -183,7 +182,7 @@ victory updater_computation::update_adv(adversary_vertex *v) {
             v->win = result;
         }
     } else if (v->leaf != leaf_type::nonleaf) {
-        // Deal with non-leaves.
+        // Deal with leaves.
 
         if (v->leaf == leaf_type::assumption) // This if condition triggers only during expansion.
         {
