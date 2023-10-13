445453129b96772df11d57c89ed6b3e218d4c552
Martin Böhm <bohm+git@atrey.karlin.mff.cuni.cz>
2023-10-13 12:59:59 +0200

    A minitool which computes all winning (or losing) amounts of sand on A and B.

---
+++ minitools/sond-on-ab.cpp
@@ -0,0 +1,108 @@
+#include <cstdio>
+#include <array>
+#include <vector>
+#include <unordered_map>
+#include <cstdint>
+
+
+#include "minitool_scale.hpp"
+#include "minibs/minibs.hpp"
+#include "minibs/minibs-three.hpp"
+#include "gs.hpp"
+
+
+
+constexpr int ONE_MINUS_TWO_ALPHA = S - 2*ALPHA;
+constexpr int TWO_MINUS_FIVE_ALPHA = 2 * S - 5 * ALPHA;
+
+
+template <int SCALE, int SPEC>
+bool sand_winning(minibs<SCALE, SPEC> &mb, const std::array<int, BINS>& sand_loads)
+{
+    itemconf<SCALE> ic;
+    ic.hashinit();
+
+    loadconf lc;
+    lc.hashinit();
+    for (int i = 0; i < BINS; i++) {
+        if (sand_loads[i] > 0) {
+            lc.assign_and_rehash(sand_loads[i], i + 1);
+        }
+    }
+
+
+    bool ret = mb.query_knownsum_layer(lc) || mb.query_itemconf_winning(lc, ic);
+
+    return ret;
+}
+
+template <int SCALE, int SPEC>
+bool sand_losing(minibs<SCALE, SPEC> &mb, const std::array<int, BINS>& sand_loads) {
+    return !sand_winning(mb, sand_loads);
+}
+
+using initial_square_matrix = std::array<std::array<bool, ONE_MINUS_TWO_ALPHA>, ONE_MINUS_TWO_ALPHA>;
+
+void print_interval(int a, int b_left_end, int b_right_end) {
+    if (b_left_end == b_right_end) {
+        fprintf(stderr, "[%d, %d] ", a, b_left_end);
+    } else {
+        fprintf(stderr, "[%d, %d-%d] ", a, b_left_end, b_right_end);
+    }
+}
+void print_square(const initial_square_matrix & m) {
+    for (int a = ONE_MINUS_TWO_ALPHA-1; a >= 0; a--) {
+        int interval_left_end = -1;
+        bool something_printed = false;
+        for (int b = 0; b <= a; b++) {
+            if (m[a][b]) {
+                if (interval_left_end == -1) {
+                    interval_left_end = b;
+                }
+            } else {
+                if (interval_left_end != -1) {
+                    print_interval(a, interval_left_end, b-1);
+                    interval_left_end = -1;
+                    something_printed = true;
+                }
+            }
+        }
+        // Close the current interval, too.
+        if (interval_left_end != -1) {
+            print_interval(a, interval_left_end, a);
+            interval_left_end = -1;
+            something_printed = true;
+        }
+
+        if(something_printed) {
+            fprintf(stderr, "\n");
+        }
+    }
+}
+
+template <int SCALE, int SPEC> void compute_sand_square(minibs<SCALE, SPEC> &mb) {
+    initial_square_matrix m = {0};
+    std::array<int, BINS> sand = {0};
+
+    for (int a = ONE_MINUS_TWO_ALPHA-1; a >= 0; a--) {
+        for (int b = 0; b <= a; b++) {
+            sand[0] = a;
+            sand[1] = b;
+            m[a][b] = sand_losing(mb, sand);
+        }
+    }
+
+    print_square(m);
+}
+
+int main(int argc, char **argv) {
+
+    zobrist_init();
+
+    minibs<MINITOOL_MINIBS_SCALE, BINS> mb;
+    mb.backup_calculations();
+
+    compute_sand_square(mb);
+
+    return 0;
+}
